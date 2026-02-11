17cfaa0799b01f9b230bffbefde53847afdc0977
Kedarini <kedardamps@gmail.com>
2026-02-11 18:40:39 +0100

    Fixed functionality of script from README.md

--- src/calculator/__main__.py
+++ src/calculator/__main__.py
@@ -1,22 +1,10 @@
-# src/calculator/__main__.py
-
-import sys
 import tkinter as tk
-
-from src.calculator.ui import CalculatorGUI
-
+from .ui import CalculatorGUI
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1] in ("--help", "-h"):
-        print("Scientific Calculator")
-        print("Usage: python -m calculator")
-        return 0
-
     root = tk.Tk()
-    app = CalculatorGUI(root)
+    CalculatorGUI(root)
     root.mainloop()
-    return 0
-
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
